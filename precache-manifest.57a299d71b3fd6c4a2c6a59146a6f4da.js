3c6a26bd40795da5762c005a95acff2bae420399
inputoutput <kopanom@qubitengineering.co.za>
2020-04-05 01:56:42 +0200

    Updates

---
+++ precache-manifest.57a299d71b3fd6c4a2c6a59146a6f4da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31c914f5ffd6ba17faec",
+    "url": "/ayoba/static/css/main.38ba9ece.chunk.css"
+  },
+  {
+    "revision": "31c914f5ffd6ba17faec",
+    "url": "/ayoba/static/js/main.31c914f5.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/ayoba/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "9c40785543021ff13021",
+    "url": "/ayoba/static/js/runtime~main.9c407855.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/ayoba/static/media/logo.05a3e30f.svg"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/ayoba/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "f38a69869fa124a4eac99306fb86244d",
+    "url": "/ayoba/index.html"
+  }
+];
